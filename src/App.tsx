c18dad64ebf4697cfaf77ceffa09fc3cded2d944
kashdevine <kasheen.mowatt@gmail.com>
2022-05-21 23:04:58 -0700

    Created the routing for the app

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,51 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import AdminBiographies from './components/pages/AdminBiographies';
+import AdminBiography from './components/pages/AdminBiography';
+import AdminProject from './components/pages/AdminProject';
+import AdminProjectImage from './components/pages/AdminProjectImage';
+import AdminProjectImages from './components/pages/AdminProjectImages';
+import AdminProjectLead from './components/pages/AdminProjectLead';
+import AdminProjectLeads from './components/pages/AdminProjectLeads';
+import AdminProjects from './components/pages/AdminProjects';
+import AdminProjectVideo from './components/pages/AdminProjectVideo';
+import AdminProjectVideos from './components/pages/AdminProjectVideos';
+import Contact from './components/pages/Contact';
+import Dashboard from './components/pages/Dashboard';
+import Home from './components/pages/Home';
+import Login from './components/pages/Login';
+import Project from './components/pages/Project';
+import Projects from './components/pages/Projects';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='contact' element={<Contact/>}/>
+        <Route path='login' element={<Login/>}/>
+        <Route path='projects' element={<Projects/>}>
+          <Route path=':id' element={<Project/>}/>
+        </Route>
+        <Route path='dashboard' element={<Dashboard/>}>
+          <Route path='projects' element={<AdminProjects/>}>
+            <Route path=':id' element={<AdminProject/>}/>
+          </Route>
+          <Route path='images' element={<AdminProjectImages/>}>
+            <Route path=':id' element={<AdminProjectImage/>}/>
+          </Route>
+          <Route path='videos' element={<AdminProjectVideos/>}>
+            <Route path=':id' element={<AdminProjectVideo/>}/>
+          </Route>
+          <Route path='bios' element={<AdminBiographies/>}>
+            <Route path=':id' element={<AdminBiography/>}/>
+          </Route>
+          <Route path='leads' element={<AdminProjectLeads/>}>
+            <Route path=':id' element={<AdminProjectLead/>}/>
+          </Route>
+        </Route>
+      </Routes>
+    </Router>
   );
 }
 
